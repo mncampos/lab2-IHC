584d1857c38e9579d2e2664a4c27ee54347e1c36
Mateus Nunes Campos <nunesmaat@gmail.com>
2021-09-06 19:30:26 -0300

    atividade 1 completa

--- lab1-ihc-exercicio1/App.js
+++ lab1-ihc-exercicio1/App.js
@@ -1,14 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Appbar, TextInput, Button } from 'react-native-paper';
 
 export default function App() {
+
+  const [number1, setNumber1] = React.useState('');
+  const [number2, setNumber2] = React.useState('');
+  const [result, setResult] = React.useState('');
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <View>
+    <Appbar.Header>
+        <Appbar.Content title="Atividade 1"/>
+    </Appbar.Header>
+
+    <TextInput label="First number" value={number1} onChangeText={ (number1) => setNumber1(number1)} />
+    <TextInput label="Second number" value={number2} onChangeText={ (number2) => setNumber2(number2)} />
+    <Button mode="contained" onPress={() => setResult(parseInt(number1)+parseInt(number2))}>
+      SUM
+    </Button>
+    <Text>
+      RESULT = {result}
+    </Text>
+
+</View>
+);
+}
+
+
+
+function SomaNumero()
+{
+
 }
 
 const styles = StyleSheet.create({

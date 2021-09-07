ce5c930af34a00ca45cf361f81de14c779c73a79
Mateus Nunes Campos <nunesmaat@gmail.com>
2021-09-06 22:00:26 -0300

    atividade 2 completa

--- lab1-ihc-exercicio2/App.js
+++ lab1-ihc-exercicio2/App.js
@@ -1,15 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Appbar, TextInput, Button } from 'react-native-paper';
+import { NavigationContainer, StackActions} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Tela 1"
+          component={HomeScreen}
+          options={{ title : 'Atividade 2', headerStyle: {
+            backgroundColor:  '#6200ee',
+          },
+          headerTitleStyle: {
+            color: 'white'
+          }
+        }}
+          />
+          <Stack.Screen name="Tela 2" 
+          component={ProfileScreen}
+          options={{ title: 'Atividade 2', headerStyle: {
+            backgroundColor: '#6200ee',
+          },
+          headerTitleStyle: {
+            color: 'white'
+          }
+          }}  />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
+const HomeScreen = ({ navigation }) => {
+
+  const [message, setMessage] = React.useState('');
+
+  return (
+
+    <View> 
+    <TextInput label="Enter a message" value={message} onChangeText={ (message) => setMessage(message)}/>
+    <Button
+      title="Send information"
+      onPress={() =>
+        navigation.navigate('Tela 2', { mensagem: message })
+      }>
+      SEND
+    </Button>
+    </View>
+  );
+};
+const ProfileScreen = ({ navigation, route }) => {
+  return <Text>{route.params.mensagem}</Text>;
+};
 
 const styles = StyleSheet.create({
   container: {
